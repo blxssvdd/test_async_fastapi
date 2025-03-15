05e5115cec233eddb6f7739484e6f471112bb075
blessed <ytminicraftyt@gmail.com>
2025-03-15 10:37:42 +0200

    upd2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Query
 import uvicorn
 
-from data import db
+import data
 
 
 app = FastAPI()
+users = []
 
 
 @app.get("/")
@@ -12,28 +13,29 @@ async def index():
     return dict(msg="Вітаємо в нашій інформаційній системі")
 
 
+
+@app.get("/get_users/")
+def get_users():
+    return {"users": users}
+
+
+
 @app.post("/add_user/")
-async def add_user(name: str = Query(description="Введіть своє ім'я"), age: int = Query(None, description="Введіть свій вік")):
-    if name in db:
-        return dict(msg="Користувач з таким іменем існує")
-
-    db.update({name: age})
-    return dict(msg="Дані успішно оновлено")
+async def add_user(name: str = Query(description="Введіть своє ім'я")):
+     if name not in data.db:
+        users.append(name)
+        return dict(msg="Дані додано.")
 
 
-@app.get("/users/")
-async def get_users():
-    return dict(msg="Запит дозволено", users=db)
 
+@app.delete("/del_user/")
+def del_user(name: str = Query(description="Введіть ім'я для видалення")):
+    if name not in users:
+        return dict(msg="Немає такого ім'я")
+    users.remove(name)
+    return dict(msg="Ім'я видалено.")
 
-@app.get("/user/")
-async def get_user(name: str = Query(description="Введіть ім'я для пошуку")):
-    if not name in db:
-        return dict(msg="Такого користувача не знайдено")
-
-    return dict(msg=f"Користувач з іменем {name} має вік {db[name]}")
 
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
-
